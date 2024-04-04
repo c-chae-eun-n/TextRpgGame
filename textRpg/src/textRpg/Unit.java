13ebb8050a38d42cd38d3f751056df9eaa47a102
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-04 16:16:23 +0900

    Add : variable

--- textRpg/src/textRpg/Unit.java
+++ textRpg/src/textRpg/Unit.java
@@ -1,5 +1,11 @@
 package textRpg;
 
 public class Unit {
-
+	private String name;
+	private int level, hp, maxHp, power, def, exp;
+	private boolean isStun, isShield;
+	private boolean party;
+	private Item weapon, armor, ring, potion;
+	
+	
 }
