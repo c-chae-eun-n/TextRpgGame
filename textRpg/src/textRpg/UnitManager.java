d909c3003a2efa482fa2da1241c64bae5e1be0a9
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-04 17:32:50 +0900

    Add : init method

--- textRpg/src/textRpg/UnitManager.java
+++ textRpg/src/textRpg/UnitManager.java
@@ -1,11 +1,18 @@
 package textRpg;
 
+import java.util.Random;
 import java.util.Vector;
 
 public class UnitManager {
 	
+	private Random random = new Random();
+	private Player player = new Player();
+	
 	private Vector<Player> playerList = new Vector<>();
 	private Vector<Monster> monsterList = new Vector<>();
+	
+	private String path = "textRpg.";
+	private String monsters[] = {"MonsterGhost","MonsterWolf","MonsterBat"};
 
 	private UnitManager() {
 		
@@ -15,5 +22,9 @@ public class UnitManager {
 	
 	public UnitManager getInstance() {
 		return instance;
+	}
+	
+	public void init() {
+		player.init();
 	}
 }
