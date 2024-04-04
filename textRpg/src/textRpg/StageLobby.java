9dc3dbe4f24b24feabe5b1a2fe045d9aff174d8d
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-04 16:09:51 +0900

    Update : update method

--- textRpg/src/textRpg/StageLobby.java
+++ textRpg/src/textRpg/StageLobby.java
@@ -4,7 +4,23 @@ public class StageLobby extends Stage {
 
 	@Override
 	public boolean update() {
-		// TODO Auto-generated method stub
+		System.out.println("~~~~~~~~~~~~ [🏰성 내부🏰] ~~~~~~~~~~~~");
+		System.out.println();
+		System.out.println(" [1]전투하러가기⚔️ [2]준비하기🔧‍ [3]종료하기🏴");
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		int sel = 0;
+		while(sel != 1 && sel != 2 && sel != 3) {
+			sel = GameManager.scan.nextInt();
+			
+			if(sel == 1) 
+				GameManager.nextStage = "BATTLE";
+			else if(sel == 2) 
+				GameManager.nextStage = "SETTING";
+			else if(sel == 3) 
+				GameManager.nextStage = "";
+		}
 		return false;
 	}
 
