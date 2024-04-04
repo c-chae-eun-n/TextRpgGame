c65fd0768890b7ffe60c7ad29e2b48c2fb104e30
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-04 14:26:22 +0900

    Modify : StageBoss Class

--- textRpg/src/textRpg/StageBoss.java
+++ textRpg/src/textRpg/StageBoss.java
@@ -1,5 +1,17 @@
 package textRpg;
 
-public class StageBoss {
+public class StageBoss extends Stage {
+
+	@Override
+	public boolean update() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
