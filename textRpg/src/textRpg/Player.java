560d167ae1e80a8528188e5c5ef0bf860621488a
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-04 18:21:05 +0900

    Update : Player class init method

--- textRpg/src/textRpg/Player.java
+++ textRpg/src/textRpg/Player.java
@@ -3,8 +3,12 @@ package textRpg;
 public class Player extends Unit {
 	
 	public static int money;
+	public static Guild guild = new Guild();
+	public static Inventory inven = new Inventory();
+	
+	
 
 	public void init() {
-		
+		money = 1000;
 	}
 }
