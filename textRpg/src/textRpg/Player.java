ed56cd4c1b7df9a3b04ea22b9eaae287ce6a55ff
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-04 18:24:10 +0900

    Add : Player class constructor

--- textRpg/src/textRpg/Player.java
+++ textRpg/src/textRpg/Player.java
@@ -6,9 +6,15 @@ public class Player extends Unit {
 	public static Guild guild = new Guild();
 	public static Inventory inven = new Inventory();
 	
+	public Player() {}
+	
+	public Player(String name, int level, int maxHp, int power, int def, int exp) {
+		super(name, level, maxHp, power, def, exp);
+	}
 	
 
 	public void init() {
 		money = 1000;
 	}
+	
 }
