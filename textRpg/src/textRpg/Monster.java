e7e647c3e62d6379fdb36b3b5d4988932f7efbc3
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-04 17:35:37 +0900

    Add : constructor

--- textRpg/src/textRpg/Monster.java
+++ textRpg/src/textRpg/Monster.java
@@ -1,5 +1,17 @@
 package textRpg;
 
 public class Monster {
-
+	private int hp, maxHp, power;
+	private String name;
+	
+	public Monster() {
+		
+	}
+	
+	public Monster(String name, int maxHp, int power) {
+		this.name = name;
+		this.maxHp = maxHp;
+		this.hp = maxHp;
+		this.power = power;
+	}
 }
