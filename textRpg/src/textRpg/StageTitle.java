4a2850597df5ffc1de0190af612f78b066f9f619
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-04 15:46:43 +0900

    Update : update & style only

--- textRpg/src/textRpg/StageTitle.java
+++ textRpg/src/textRpg/StageTitle.java
@@ -4,14 +4,61 @@ public class StageTitle extends Stage {
 
 	@Override
 	public boolean update() {
-		// TODO Auto-generated method stub
+		printHouse();
+		try {
+			Thread.sleep(1000);
+		} catch (Exception e) {
+		}
+		printMessage();
+		try {
+			Thread.sleep(1000);
+		} catch (Exception e) {
+		}
+		printContent();
+		System.out.println("[1] 성으로 간다  [2] 성으로 가지 않는다");
+		int sel = GameManager.scan.nextInt();
+		if(sel == 1) {
+			printCastle();
+			GameManager.nextStage = "LOBBY";
+			return false;
+		}
+		System.out.println("🤷🏻‍♀️나 말고 누군가가 구해주겠지..~~🤷🏻‍♂️");
+		GameManager.nextStage = "";
 		return false;
 	}
 
 	@Override
-	public void init() {
-		// TODO Auto-generated method stub
-		
-	}
+	public void init() {}
 
+	private void printHouse() {
+		System.out.println();
+		System.out.println("어느 숲속마을.....");
+		System.out.println("🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳");
+		System.out.println("🌳🌳🌳🌳🌳🌳🌳🌳🏠🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳");
+		System.out.println("🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳");
+	}
+	
+	private void printMessage() {
+		System.out.println();
+		System.out.println("🍃🍃➰️〰️🍃🍂🍃🍃➰〰️〰️📨");
+		System.out.println("편지가 날아왔다!!");
+	}
+	
+	private void printContent() {
+		System.out.println("------------------------------------");
+		System.out.println("|                                  |");
+		System.out.println("| 📢 납치당한 공주를 구할 ⚔️용사⚔️ 찾습니다  |");
+		System.out.println("| 📢 용감한 용사들은 모두 성으로 모여주세요!! |");
+		System.out.println("|                                  |");
+		System.out.println("------------------------------------");
+	}
+	
+	private void printCastle() {
+		System.out.println("성으로 가는 길이 나타났다..!!");
+		System.out.println("🌳🌳🌳🌳🌌🌌🌳🌳🌳🌳🌳🌳🌳🌳");
+		System.out.println("🌳🌳🌳🌳🌳🌌🌳🌳🌳🌳🌳🌳🌳🌳");
+		System.out.println("🌳🌳🌳🌳🌳🌌🌌🌌🌳🌳🌳🌳🌳🌳");
+		System.out.println("🌳🌳🌳🌳🌳🌳🌳🌌🌳🌳🌳🌳🌳🌳");
+		System.out.println("🧱🧱🧱🧱🧱🧱🧱🏰🧱🧱🧱🧱🧱🧱");
+	}
 }
