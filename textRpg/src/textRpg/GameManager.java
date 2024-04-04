e530cfcccae16c89790c368fb903b73e6b7f6fbf
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-04 15:23:06 +0900

    Add : init 메소드 추가

--- textRpg/src/textRpg/GameManager.java
+++ textRpg/src/textRpg/GameManager.java
@@ -1,8 +1,23 @@
 package textRpg;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class GameManager {
 	public static Scanner scan = new Scanner(System.in);
 	public static String nextStage = "";
+	private String curStage = "";
+	
+	Map<String, Stage> stageList = new HashMap<String, Stage>();
+	
+	private void init() {
+		stageList.put("TITLE", new StageTitle());
+		stageList.put("BATTLE", new StageBattle());
+		stageList.put("LOBBY", new StageLobby());
+		stageList.put("SETTING", new StageSetting());
+		stageList.put("BOSS", new StageBoss());
+		nextStage = "TITLE";
+	}
+
 }
