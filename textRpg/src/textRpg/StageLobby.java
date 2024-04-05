61a093e7f0c8c6fdbd5e2f2cc1e6b72bc1eb0c19
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-05 10:59:45 +0900

    Update : StageLobby class update method

--- textRpg/src/textRpg/StageLobby.java
+++ textRpg/src/textRpg/StageLobby.java
@@ -1,21 +1,24 @@
 package textRpg;
 
 public class StageLobby extends Stage {
+	private final int BATTLE = 1;
+	private final int SETTING = 2;
+	private final int EXIT = 0;
 
 	@Override
 	public boolean update() {
 		System.out.println("~~~~~~~~~~~~ [🏰성 내부🏰] ~~~~~~~~~~~~\n");
-		System.out.println(" [1]전투하러가기⚔️ [2]준비하기🔧‍ [3]종료하기🏴\n");
+		System.out.println(" [1]전투하러가기⚔️ [2]준비하기🔧‍ [0]종료하기🏴\n");
 		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
-		int sel = 0;
-		while(sel != 1 && sel != 2 && sel != 3) {
+		int sel = -1;
+		while(sel != BATTLE && sel != SETTING && sel != EXIT) {
 			sel = GameManager.scan.nextInt();
 			
-			if(sel == 1) 
+			if(sel == BATTLE) 
 				GameManager.nextStage = "BATTLE";
-			else if(sel == 2) 
+			else if(sel == SETTING) 
 				GameManager.nextStage = "SETTING";
-			else if(sel == 3) 
+			else if(sel == EXIT) 
 				GameManager.nextStage = "";
 		}
 		return false;
