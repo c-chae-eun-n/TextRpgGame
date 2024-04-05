7af8c73fb34ac974823ffd698e1c6eb2c67fde60
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-05 18:35:46 +0900

    Delete : Item class printSetItemMenu method

--- textRpg/src/textRpg/Item.java
+++ textRpg/src/textRpg/Item.java
@@ -32,12 +32,5 @@ public class Item {
 	public String getName() {
 		return name;
 	}
-	
-	public void printSetItemMenu() {
-		System.out.println(GameManager.red + "~~~~~~~~~~~~~ [⛔관리자⛔] ~~~~~~~~~~~~\n");
-		System.out.println(" [1]무기        [2]갑옷       [3]장신구\n");
-		System.out.println(" [4]포션        [0]뒤로가기\n");
-		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" + GameManager.exit);
-	}
 
 }
