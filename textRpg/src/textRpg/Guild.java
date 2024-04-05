23af29f1d8512571862b0a4a84893f8c2526eb35
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-05 11:25:20 +0900

    Add : Guild class printPlayer method

--- textRpg/src/textRpg/Guild.java
+++ textRpg/src/textRpg/Guild.java
@@ -34,13 +34,13 @@ public class Guild {
 	public void guildMenu() {
 		while(true) {
 			System.out.println("~~~~~~~~~~~~~ [⚜️길드⚜️] ~~~~~~~~~~~~~\n");
-			System.out.println(" [1]길드 목록  [2]길드원 추가  [3]길드원 삭제\n");
-			System.out.println(" [4]파티 모집  [5]파티원 교체  [0]종료\n");
+			System.out.println(" [1]길드 목록  [2]길드원 모집  [3]길드원 삭제\n");
+			System.out.println(" [4]파티 모집  [5]파티원 교체  [0]뒤로가기\n");
 			System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
 			
 			int sel = GameManager.scan.nextInt();
 			if(sel == LIST) {
-				
+				printPlayerStatusAll();
 			}
 			else if(sel == ADD) {
 				
@@ -58,5 +58,22 @@ public class Guild {
 				break;
 			}
 		}
+	}
+	
+	private void printPlayerStatusAll() {
+		if(guildList.size() == 0) {
+			System.err.println("⚜️길드원⚜️이 없습니다!\n⚜️길드원⚜️을 모집해서 ⚜️길드⚜️를 만들어보세요!\n");
+			return;
+		}
+		for(int i=0; i<guildList.size(); i++) {
+			Player player = guildList.get(i);
+			System.out.println("~~~~~~~~~~~~~ [⚜️길드원⚜️] ~~~~~~~~~~~~\n");
+			player.printStatus();
+			System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+		}
+	}
+	
+	private void printPlayerStatus(int sel) {
+		guildList.get(sel).printStatus();
 	}
 }
