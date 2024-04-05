99ccefc96de441098247def6641966e52f115041
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-05 10:13:18 +0900

    Add : StageSetting class constant

--- textRpg/src/textRpg/StageSetting.java
+++ textRpg/src/textRpg/StageSetting.java
@@ -1,10 +1,16 @@
 package textRpg;
 
 public class StageSetting extends Stage {
+	private final int  GUILD = 1;
+	private final int SHOP = 2;
+	private final int INVENTORY = 3;
+	private final int SAVE = 4;
+	private final int LOAD = 5;
+	private final int EXIT = 0;
 
 	@Override
 	public boolean update() {
-		// TODO Auto-generated method stub
+
 		return false;
 	}
 
