0d6ed7992e84179bfdcdf52795e56e479f46666d
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-05 17:46:05 +0900

    Delete : Item class collection & setItem method

--- textRpg/src/textRpg/Item.java
+++ textRpg/src/textRpg/Item.java
@@ -1,7 +1,5 @@
 package textRpg;
 
-import java.util.ArrayList;
-
 public class Item {
 	public static final int WEAPON = 1;
 	public static final int AMOR = 2;
@@ -11,8 +9,6 @@ public class Item {
 	private int kind, power, price;
 	private String name;
 	
-	private ArrayList<Item> itemList = new ArrayList<>();
-	
 	public Item(String name, int kind, int power, int price) {
 		this.kind = kind;
 		this.power = power;
@@ -21,28 +17,9 @@ public class Item {
 	}
 	
 	private void printSetItemMenu() {
-		System.out.println("~~~~~~~~~~~~~ [✨아이템✨] ~~~~~~~~~~~~\n");
+		System.out.println(GameManager.red + "~~~~~~~~~~~~~ [⛔관리자⛔] ~~~~~~~~~~~~\n");
 		System.out.println(" [1]무기        [2]갑옷       [3]장신구\n");
 		System.out.println(" [4]포션        [0]뒤로가기\n");
-		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
-	}
-	
-	public void setItem() {
-		while(true) {
-			printSetItemMenu();
-			int sel = GameManager.scan.nextInt();
-			if(sel == WEAPON) {
-				
-			}
-			else if(sel == AMOR) {
-				
-			}
-			else if(sel == RING) {
-				
-			}
-			else if(sel == POTION) {
-				
-			}
-		}
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" + GameManager.exit);
 	}
 }
