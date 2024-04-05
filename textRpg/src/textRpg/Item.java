ead42ef042d6fed58d7a56cb9d00cf275bf391e0
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-05 16:35:35 +0900

    Add : Item class constant & variable

--- textRpg/src/textRpg/Item.java
+++ textRpg/src/textRpg/Item.java
@@ -1,5 +1,11 @@
 package textRpg;
 
 public class Item {
-
+	public static final int WEAPON = 1;
+	public static final int AMOR = 2;
+	public static final int RING = 3;
+	public static final int POTION = 4;
+	
+	private int kind, power, price;
+	private String name;
 }
