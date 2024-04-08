03229f43354d1fd914432fffd37ef5892da55ca3
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-08 11:55:24 +0900

    Modify : Inventory class collection & construct singleton

--- textRpg/src/textRpg/Inventory.java
+++ textRpg/src/textRpg/Inventory.java
@@ -1,5 +1,17 @@
 package textRpg;
 
-public class Inventory {
+import java.util.ArrayList;
 
+public class Inventory {
+	private ArrayList<Item> invenList;
+	
+	private Inventory() {
+		invenList = new ArrayList<>();
+	}
+	
+	private static Inventory instance = new Inventory();
+	
+	public static Inventory getInstance() {
+		return instance;
+	}
 }
