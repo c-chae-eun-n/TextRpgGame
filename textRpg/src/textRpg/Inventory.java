486c8c2ac6ad3bcc7e70edb02ee96d5a83e8e653
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-08 15:46:16 +0900

    Add : Inventory class inventoryMenu method

--- textRpg/src/textRpg/Inventory.java
+++ textRpg/src/textRpg/Inventory.java
@@ -15,6 +15,24 @@ public class Inventory {
 		return instance;
 	}
 	
+	public void inventoryMenu() {
+		while(true) {
+			System.out.println("~~~~~~~~~~~~ [🎒인벤토리🎒] ~~~~~~~~~~~~\n");
+			System.out.println(" [1]착용🪞     [2]판매💰    [0]뒤로가기🔙\n");
+			System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+			int sel = GameManager.scan.nextInt();
+			if(sel == 1) {
+				
+			}
+			else if(sel == 2) {
+				
+			}
+			else if(sel == 0) {
+				break;
+			}
+		}
+	}
+	
 	public void create(Item item) {
 		invenList.add(item);
 		System.out.println(" 📦"+item.getName()+" 아이템📦 구매가 완료되었습니다.\n");
