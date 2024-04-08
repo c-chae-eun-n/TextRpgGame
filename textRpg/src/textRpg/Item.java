c90e7e2d244212a491e8ce79d70b14c6cfdda1b9
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-08 10:41:36 +0900

    Update : Item class AMOR constant -> ARMOR (오타수정)

--- textRpg/src/textRpg/Item.java
+++ textRpg/src/textRpg/Item.java
@@ -2,7 +2,7 @@ package textRpg;
 
 public class Item {
 	public static final int WEAPON = 1;
-	public static final int AMOR = 2;
+	public static final int ARMOR = 2;
 	public static final int RING = 3;
 	public static final int POTION = 4;
 	public static final int ADMIN = 5;
