92dd447657e29ee79903898b843948f4829554f3
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-08 10:16:39 +0900

    Update : UnitManager class random -> GameManager static random class

--- textRpg/src/textRpg/UnitManager.java
+++ textRpg/src/textRpg/UnitManager.java
@@ -1,11 +1,9 @@
 package textRpg;
 
-import java.util.Random;
 import java.util.Vector;
 
 public class UnitManager {
 	
-	private Random random = new Random();
 	public Player player = new Player();
 	
 	private Vector<Player> playerList = new Vector<>();
@@ -30,14 +28,14 @@ public class UnitManager {
 	
 	public void monsterRandomSet(int size) {
 		for(int i=0; i<size; i++) {
-			int rIdx = random.nextInt(monsters.length);
+			int rIdx = GameManager.random.nextInt(monsters.length);
 			try {
 				Class<?> clazz = Class.forName(path + monsters[rIdx]);
 				Object obj = clazz.getDeclaredConstructor().newInstance();
 				Monster monster = (Monster) obj;
 				
-				int hp = random.nextInt(100) + 100;
-				int power = random.nextInt(10) + 10;
+				int hp = GameManager.random.nextInt(100) + 100;
+				int power = GameManager.random.nextInt(10) + 10;
 				monster.init(hp, power);
 				monsterList.add(monster);
 			} catch (Exception e) {
