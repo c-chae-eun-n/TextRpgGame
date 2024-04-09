55a3799bc83d95efef18f03e6a1406b7dc00e794
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-09 12:01:55 +0900

    Delete : Item class POTION constant

--- textRpg/src/textRpg/Item.java
+++ textRpg/src/textRpg/Item.java
@@ -4,8 +4,7 @@ public class Item {
 	public static final int WEAPON = 1;
 	public static final int ARMOR = 2;
 	public static final int RING = 3;
-	public static final int POTION = 4;
-	public static final int ADMIN = 5;
+	public static final int ADMIN = 4;
 	
 	private int kind, power, price;
 	private String name;
