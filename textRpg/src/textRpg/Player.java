2354d0e7c8fb4dee7f18921036cb861189393346
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-09 11:35:52 +0900

    Add : Player class invenMenu, getInventory method

--- textRpg/src/textRpg/Player.java
+++ textRpg/src/textRpg/Player.java
@@ -3,12 +3,13 @@ package textRpg;
 public class Player extends Unit {
 	
 	public static Guild guild = Guild.getInstance();
-	public static Inventory inven = Inventory.getInstance();
+	private Inventory inven;
 	
 	public Player() {}
 	
 	public Player(String name, int level, int maxHp, int power, int def, int exp) {
 		super(name, level, maxHp, power, def, exp);
+		inven = new Inventory();
 	}
 	
 	@Override
@@ -19,5 +20,13 @@ public class Player extends Unit {
 	
 	public void guildMenu() {
 		guild.guildMenu();
+	}
+	
+	public void invenMenu() {
+		inven.inventoryMenu();
+	}
+	
+	public Inventory getInventory() {
+		return this.inven;
 	}
 }
