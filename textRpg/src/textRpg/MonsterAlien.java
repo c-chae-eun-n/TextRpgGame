d5ab3239893bb9d083ca8ef20d6a3c4d7a5c5a6f
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-09 18:21:25 +0900

    Update : MonsterAlien class skill method

--- textRpg/src/textRpg/MonsterAlien.java
+++ textRpg/src/textRpg/MonsterAlien.java
@@ -7,7 +7,13 @@ public class MonsterAlien extends Monster {
 
 	@Override
 	public void skill(Player target) {
-		
+		System.out.println(GameManager.yellow + " [" + getName() + "] 물어뜯기-!");
+		target.setHp(target.getHp()-(getPower()*2));
+		System.out.println(" ["+getName()+"] ["+target.getName()+"]에게 "+(getPower()*2)+" 데미지를 입혔다!\n" + GameManager.exit);
+		if(target.getHp() <= 0) {
+			System.out.printf(GameManager.red + "          [%s] 🪦사망🪦\n\n", target.getName() + GameManager.exit);
+			target.setHp(0);
+		}
 	}
 
 }
