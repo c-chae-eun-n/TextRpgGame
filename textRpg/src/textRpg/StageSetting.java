b96845b8f9d85c2c8ebb61c45fd3069538a109f5
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-09 11:39:24 +0900

    Add : StageSetting class tempLogin method

--- textRpg/src/textRpg/StageSetting.java
+++ textRpg/src/textRpg/StageSetting.java
@@ -8,39 +8,48 @@ public class StageSetting extends Stage {
 	private final int LOAD = 5;
 	private final int EXIT = 0;
 	
-	private UnitManager unitManager = UnitManager.getInstance();
+//	private UnitManager unitManager = UnitManager.getInstance();
 	private Shop shop = Shop.getInstance();
+	public static Player player = null;
+	private boolean isValid = true;
 
 	@Override
 	public boolean update() {
-		while(true) {
-			System.out.println("~~~~~~~~~~~~~ [🔧설정🔧] ~~~~~~~~~~~~~\n");
-			System.out.println(" [1]길드⚜️     [2]상점🛒    [3]인벤토리🎒\n");
-			System.out.println(" [4]저장📁     [5]로드📂    [0]뒤로가기🔙\n");
-			System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-			System.out.print(" 메뉴 번호 입력 : ");
-			
-			int sel = GameManager.scan.nextInt();
-			if(sel == GUILD) {
-				unitManager.player.guildMenu();
-			}
-			else if(sel == SHOP) {
-				shop.shopMenu();
-			}
-			else if(sel == INVENTORY) {
-				Player.inven.inventoryMenu();
-			}
-			else if(sel == SAVE) {
+		if(player == null)
+			tempLogin();
+		if(isValid) {
+			while(true) {
+				System.out.println("~~~~~~~~~~~~~ [🔧설정🔧] ~~~~~~~~~~~~~\n");
+				System.out.println(" [1]길드⚜️     [2]상점🛒    [3]인벤토리🎒\n");
+				System.out.println(" [4]저장📁     [5]로드📂    [0]뒤로가기🔙\n");
+				System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+				System.out.print(" 메뉴 번호 입력 : ");
 				
+				int sel = GameManager.scan.nextInt();
+				if(sel == GUILD) {
+					player.guildMenu();
+				}
+				else if(sel == SHOP) {
+					shop.shopMenu();
+				}
+				else if(sel == INVENTORY) {
+					player.invenMenu();
+				}
+				else if(sel == SAVE) {
+					
+				}
+				else if(sel == LOAD) {
+					
+				}
+				else if(sel == EXIT) {
+					GameManager.nextStage = "LOBBY";
+					player = null;
+					break;
+				}
 			}
-			else if(sel == LOAD) {
-				
-			}
-			else if(sel == EXIT) {
-				GameManager.nextStage = "LOBBY";
-				break;
-			}
+			return false;
 		}
+		GameManager.nextStage = "LOBBY";
 		return false;
 	}
 
@@ -49,5 +58,14 @@ public class StageSetting extends Stage {
 		// TODO Auto-generated method stub
 		
 	}
-
+	
+	public void tempLogin() {
+		System.out.print(" 🔧설정🔧 사용 할 플레이어의 이름 입력 : ");
+		String name = GameManager.scan.next();
+		player = Player.guild.findPlayerByName(name);
+		if(player == null) {
+			System.err.println("존재하지 않는 플레이어입니다.");
+			isValid = false;
+		}
+	}
 }
