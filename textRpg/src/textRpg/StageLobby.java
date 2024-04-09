690558b8446aaa934ff39f9c0ee0c85e9506d40e
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-09 11:13:13 +0900

    Add : StageLobby class tempLogin method

--- textRpg/src/textRpg/StageLobby.java
+++ textRpg/src/textRpg/StageLobby.java
@@ -4,30 +4,48 @@ public class StageLobby extends Stage {
 	private final int BATTLE = 1;
 	private final int SETTING = 2;
 	private final int EXIT = 0;
+	
+	private Guild guild = Guild.getInstance();
+	public static Player player = null;
+	private boolean isValid = true;
 
 	@Override
 	public boolean update() {
 		System.out.println("~~~~~~~~~~~~ [🏰성 내부🏰] ~~~~~~~~~~~~\n");
-		System.out.println(" [1]전투하러가기⚔️ [2]준비하기🔧‍ [0]종료하기🏴\n");
-		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
-		int sel = -1;
-		while(sel != BATTLE && sel != SETTING && sel != EXIT) {
-			sel = GameManager.scan.nextInt();
-			
-			if(sel == BATTLE) 
-				GameManager.nextStage = "BATTLE";
-			else if(sel == SETTING) 
-				GameManager.nextStage = "SETTING";
-			else if(sel == EXIT) 
-				GameManager.nextStage = "";
+		if(player == null) 
+			tempLogin();
+		if(isValid) {
+			System.out.println(" [1]전투하러가기⚔️ [2]준비하기🔧‍ [0]종료하기🏴\n");
+			System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+			int sel = -1;
+			while(sel != BATTLE && sel != SETTING && sel != EXIT) {
+				sel = GameManager.scan.nextInt();
+				
+				if(sel == BATTLE) 
+					GameManager.nextStage = "BATTLE";
+				else if(sel == SETTING) 
+					GameManager.nextStage = "SETTING";
+				else if(sel == EXIT) 
+					GameManager.nextStage = "";
+			}
+			return false;
 		}
+		GameManager.nextStage = "";
 		return false;
 	}
 
 	@Override
 	public void init() {
-		// TODO Auto-generated method stub
-		
 	}
-
+	
+	private void tempLogin() {
+		System.out.print(" 용사의 이름 입력 : ");
+		String name = GameManager.scan.next();
+		player = guild.findPlayerByName(name);
+		if(player == null) {
+			System.err.println(" 📢 존재하지 않는 용사!");
+			isValid = false;
+		}
+		System.out.println("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+	}
 }
