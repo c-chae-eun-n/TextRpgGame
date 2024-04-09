47e273023f831a08f963c4b54f24782105431e3f
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-09 12:29:05 +0900

    Modify : StageBattle class

--- textRpg/src/textRpg/StageBattle.java
+++ textRpg/src/textRpg/StageBattle.java
@@ -1,7 +1,17 @@
 package textRpg;
 
+import java.util.ArrayList;
+
 public class StageBattle extends Stage {
 
+	private UnitManager unitManager = null;
+	private ArrayList<Monster> monsterList = new ArrayList<>();
+	private int monsterDead, playerDead;
+	
+	public StageBattle() {
+		unitManager = UnitManager.getInstance();
+	}
+	
 	@Override
 	public boolean update() {
 		// TODO Auto-generated method stub
