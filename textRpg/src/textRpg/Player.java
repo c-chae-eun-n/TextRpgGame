2f9a588bdc0775249e407af5e04d9fe92894f7d2
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-10 19:48:34 +0900

    Add : Player class warriorSkill method

--- textRpg/src/textRpg/Player.java
+++ textRpg/src/textRpg/Player.java
@@ -14,13 +14,13 @@ public class Player extends Unit {
 	
 	public void skill(Player p, Monster target) {
 		if(p.getKind() == Unit.WARRIOR) {
-			
+			warriorSkill(p, target);
 		}
 		else if(p.getKind() == Unit.WIZARD) {
-			
+			wizardSkill(p, target);
 		}
 		else if(p.getKind() == Unit.HEALER) {
-			
+			healerSkill(p, target);
 		}
 	}
 	
@@ -34,5 +34,25 @@ public class Player extends Unit {
 	
 	public Inventory getInventory() {
 		return this.inven;
+	}
+	
+	private void warriorSkill(Player p, Monster target) {
+		System.out.println(GameManager.yellow + " [" + getName() + "] 쉴드 발동!!");
+		p.setShield(true);
+		target.setHp(target.getHp()-getPower()/2);
+		System.out.println(" [" + target.getName() + "]가 " + "쉴드 효과에 " + getPower()/2 + " 데미지를 입었다!!\n" + GameManager.exit);
+		if(target.getHp() <= 0) {
+			System.out.println(GameManager.red + "       [" + target.getName() + "]을(를) 처치했습니다.\n" + GameManager.exit);
+			target.setHp(0);
+			p.getRandomPotion();
+		}
+	}
+	
+	private void wizardSkill(Player p, Monster target) {
+		
+	}
+	
+	private void healerSkill(Player p, Monster target) {
+		
 	}
 }
